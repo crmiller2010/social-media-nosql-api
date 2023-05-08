834851a0c0caa46c8a800c60b96d63d51c696125
crmiller2010 <114351462+crmiller2010@users.noreply.github.com>
2023-05-08 16:31:44 -0500

    Update Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -12,22 +12,32 @@ const thoughtSchema = new Schema({
         minLength: 1,
         maxLength: 280
     },
+    
+    
     createdAt: {
         type: Date,
         default: Date.now,
         get: timestamp => dateFormat(timestamp)
     },
+    
+    
     username: {
         type: String,
         required: true
     },
+    
+    
     reactions: [reactionSchema]
 },
+                                 
+                                 
 {
     toJSON: {
         virtuals: true,
         getters: true
     },
+    
+    
     id: false
 });
 
