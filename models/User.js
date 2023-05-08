62c1642eaf2f725dc9bba00fe6a334c77b57f10b
crmiller2010 <114351462+crmiller2010@users.noreply.github.com>
2023-05-08 16:32:21 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,19 +7,27 @@ const userSchema = new Schema({
         trim: true,
         unique: true
     },
+    
+    
     email: {
         type: String,
         required: true,
         unique: true,
         match: [/.+@.+\..+/]
     },
+    
+    
     thoughts: [{ 
         type: Schema.Types.ObjectId, ref: 'Thought' 
     }],
+    
+    
     friends: [{ 
         type: Schema.Types.ObjectId, ref: 'User' 
     }]
 },
+                              
+                              
 {
     toJSON: {
         virtuals: true
