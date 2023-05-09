7843d9c21b8cb72d7ac5501e4802c7615ab555ff
crmiller2010 <114351462+crmiller2010@users.noreply.github.com>
2023-05-08 19:44:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(require('./routes'));
-
-mongoose.connect("mongodb://localhost:27017/mongoosePotatoes", {
-
-});
 
 
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(` Connected on localhost:${PORT}`));
+app.listen(port3000);
