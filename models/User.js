4d2fd9d5537e624dc379f02e79d902f0f27e25f5
crmiller2010 <114351462+crmiller2010@users.noreply.github.com>
2023-05-08 19:42:06 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,29 +1,10 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    username: {
+
         type: String,
-        required: true,
-        trim: true,
-        unique: true
-    },
-    
-    
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/]
-    },
-    
-    
-    thoughts: [{ 
-        type: Schema.Types.ObjectId, ref: 'Thought' 
-    }],
-    
-    
-    friends: [{ 
-        type: Schema.Types.ObjectId, ref: 'User' 
+        r
+
     }]
 },
                               
@@ -32,13 +13,5 @@ const userSchema = new Schema({
     toJSON: {
         virtuals: true
     },
-    id: false
-});
-
-userSchema.virtual('friendCount').get(function() {
-    return this.friends.length;
-});
-
-const User = model('User', userSchema);
 
 module.exports = User;
