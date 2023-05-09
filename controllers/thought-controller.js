0071c4877000fe29c95a9659b55168276393b568
crmiller2010 <114351462+crmiller2010@users.noreply.github.com>
2023-05-08 19:37:30 -0500

    Update thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -5,65 +5,37 @@ const thoughtController = {
     
     getAllThoughts(req, res) {
         Thought.find({})
-        .populate({ path: 'reactions', select: '-__v' })
-        .select('-__v')
-        .then(dbThoughtData => res.json(dbThoughtData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
+      
     },
 
     
     getThoughtById({ params }, res) {
-        Thought.findOne({ _id: params.id })
-        .populate({ path: 'reactions', select: '-__v' })
-        .select('-__v')
-        .then(dbThoughtData => {
+        Thought.findOne()
+       
             if (!dbThoughtData) {
-                res.status(404).json({message: 'No thought found'});
-                return;
+               
             }
             res.json(dbThoughtData);
         })
         .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
-    },
-
-
+          
     createThought({ body }, res) {
-        Thought.create(body)
-        .then(dbThoughtData => {
-            User.findOneAndUpdate(
-                { _id: body.userId },
-                { $push: { thoughts: dbThoughtData._id } },
+        Th: dbThoughtData._id } },
                 { new: true }
             )
             .then(dbUserData => {
                 if (!dbUserData) {
-                    res.status(404).json({ message: 'No user found ' });
-                    return;
+                    res.
                 }
-                res.json(dbUserData);
-            })
-            .catch(err => res.json(err));
-        })
-        .catch(err => res.status(400).json(err));
-    },
+              
 
     updateThought({ params, body }, res) {
         Thought.findOneAndUpdate(
-            { _id: params.id },
+          
             body,
             { new: true }
         )
-        .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id' });
-                return;
-            }
+        .t
             res.json(dbThoughtData);
         })
         .catch(err => res.status(400).json(err));
@@ -73,16 +45,6 @@ const thoughtController = {
     
     deleteThought({ params }, res) {
         
-        Thought.findOneAndDelete({ _id: params.id })
-        .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id'});
-                return;
-            }
-            
-            User.findOneAndUpdate(
-                { username: dbThoughtData.username },
-                { $pull: { thoughts: params.id } }
             )
             .then(() => {
                 res.json({message: 'Successfully deleted the thought'});
@@ -94,29 +56,15 @@ const thoughtController = {
 
     
     addReaction({ params, body }, res) {
-        Thought.findOneAndUpdate(
-            { _id: params.thoughtId },
-            { $addToSet: { reactions: body } },
-            { new: true, runValidators: true }
-        )
+        Thought.
         .then(dbThoughtData => {
             if (!dbThoughtData) {
                 res.status(404).json({ message: 'No thought found with this id' });
-                return;
-            }
-            res.json(dbThoughtData);
-        })
-        .catch(err => res.status(500).json(err));
-    },
-
+                r
     
 
     deleteReaction({ params, body }, res) {
-        Thought.findOneAndUpdate(
-            { _id: params.thoughtId },
-            { $pull: { reactions: { reactionId: body.reactionId } } },
-            { new: true, runValidators: true }
-        )
+        Thought
         .then(dbThoughtData => {
             if (!dbThoughtData) {
                 res.status(404).json({ message: 'No thought found with this id' });
@@ -128,5 +76,5 @@ const thoughtController = {
     },
 }
 
-module.exports = thoughtController;
+
 
